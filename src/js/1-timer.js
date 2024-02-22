3179de0da4404f50a2deab8e5212e67f026d1c98
Dmytro Kasko <dimoks77@gmail.com>
2024-02-22 23:44:33 +0100

    et

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,23 +1,7 @@
-// Описаний в документації
 import flatpickr from 'flatpickr';
-
 import iziToast from 'izitoast';
-
 import 'izitoast/dist/css/iziToast.min.css';
-
-// Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
-
-
-const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
 
 const startButton = document.querySelector('button')
 const inputData = document.querySelector('input#datetime-picker');
@@ -27,25 +11,89 @@ const minutesData = document.querySelector('[data-minutes]');
 const secondsData = document.querySelector('[data-seconds]');
 const timer = document.querySelector(".timer");
 
-  function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+let userSelectedDate;
   
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
   
-    return { days, hours, minutes, seconds };
+  onClose(selectedDates) {   // Метод onClose() з об'єкта параметрів викликається щоразу під час закриття елемента інтерфейсу, який створює flatpickr.
+    userSelectedDate = selectedDates[0];
+    
+    if (userSelectedDate < Date.now()) {
+        iziToast.error({
+            message: 'Please choose a date in the future!',
+            position: 'topRight',
+        });
+        startButton.disabled = true; 
+    } 
+        else {
+            startButton.disabled = false;
+        }
+  },
+};
+
+
+let countdownInterval;
+
+function startTimer() {
+  countdownInterval = setInterval(updateTimer, 1000, userSelectedDate);
+};
+
+function updateTimer(endDate) {
+  const currentDate = new Date();
+  const remainingTime = endDate - currentDate;
+  const { days, hours, minutes, seconds } = convertMs(remainingTime);
+ 
+if (!isNaN(days) && !isNaN(hours) && !isNaN(minutes) && !isNaN(seconds)) {
+    daysData.textContent = addLeadingZero(days);
+    hoursData.textContent = addLeadingZero(hours);
+    minutesData.textContent = addLeadingZero(minutes);
+    secondsData.textContent = addLeadingZero(seconds);
   }
-  
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+  if (remainingTime <= 0) {
+    stopTimer();
+  }
+};
+
+startButton.addEventListener("click", ()=> {
+   if (userSelectedDate) startTimer(); 
+});
+
+function stopTimer() {
+  if (countdownInterval) {
+    
+  clearInterval(countdownInterval);
+
+    daysData.textContent = '00';
+    hoursData.textContent = '00';
+    minutesData.textContent = '00';
+    secondsData.textContent = '00';
+    
+    countdownInterval = null;
+  } 
+};
+
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+};
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+};
+
+flatpickr(inputData, options);
